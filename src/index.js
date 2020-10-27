3f17bbdc75dd4898290b90128ac36c48e0f3beaa
Sapountzi Ibraim <sapountzi@logismos.gr>
2020-10-27 13:57:43 +0200

    depo pwa client add manifest, basic service worker and icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 render(<App />, document.getElementById('root'))
+
+
+serviceWorkerRegistration.register();
