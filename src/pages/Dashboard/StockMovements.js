0cfb96ea5659c74e02456ebbe590fc3655d15c14
Sapountzi Ibraim <sapountzi@logismos.gr>
2020-10-30 00:21:20 +0200

    add idb promised dependency; store dashboard stock movements locally on indexeddb; retrieve them if request to server fails;

--- src/pages/Dashboard/StockMovements.js
+++ src/pages/Dashboard/StockMovements.js
@@ -4,26 +4,68 @@ import Page from 'material-ui-shell/lib/containers/Page'
 import Scrollbar from 'material-ui-shell/lib/components/Scrollbar'
 import StockTimeline from './Timeline'
 import config from "config/config"
+import { openDB } from 'idb';
 
 export default function () {
   const [movements, setMovements] = useState(null)
   const intl = useIntl()
   const endpoint = "Stocks"
+  const dbPromise = createIndexedDB();
 
-  const loadData = async () => {
+  const getStockMovements = async () => {
     fetch(`${config.apiURL + endpoint}`)
       .then(response => { console.log(response); return response.json()})
       .then(data => {
         setMovements(data);
+        saveStockDataLocally(data);
       })
       .catch((error) => {
-        console.error('Error while importing:', error);
+        console.error('Error while getting stock movements, will render local stored data:', error);
+        getLocalStockData()
+        .then(offlineData => {
+          if (!offlineData.length) {
+            return;
+          } else {
+            setMovements(offlineData);
+          }
+        });
       });
   }
 
   useEffect(() => {
-    loadData()
+    getStockMovements()
   }, [])
+
+  function createIndexedDB() {
+    if (!('indexedDB' in window)) {return null;}
+    return openDB('depo', 1, {
+      upgrade(db) {
+        db.createObjectStore('stockMovements',  {keyPath: 'id'});
+      },
+    });
+  }
+  
+  function saveStockDataLocally(stocks) {
+    if (!('indexedDB' in window)) {return null;}
+    return dbPromise.then(db => {
+      const tx = db.transaction('stockMovements', 'readwrite');
+      const store = tx.objectStore('stockMovements');
+      return Promise.all(stocks.map(movement => store.put(movement)))
+      .catch(() => {
+        tx.abort();
+        throw Error('Stock movements were not added to the store');
+      });
+    });
+  }
+  
+  function getLocalStockData() {
+    if (!('indexedDB' in window)) {return null;}
+    return dbPromise.then(db => {
+      const tx = db.transaction('stockMovements', 'readonly');
+      const store = tx.objectStore('stockMovements');
+      return store.getAll();
+    });
+  }
 
   return (
     <Page
